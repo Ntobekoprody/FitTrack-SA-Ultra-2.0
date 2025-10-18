256e93c1fe7c04e25337062b02a46dc3cd3bdc6e
Ntobekoprody <104379808+Ntobekoprody@users.noreply.github.com>
2025-10-18 22:00:57 +0200

    Add Material dependencies for XML theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,9 +56,11 @@ dependencies {
     androidTestImplementation(composeBom)
 
     implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
